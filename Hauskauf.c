be5dd4d5b568b58088d3e5255c587c7733008640
marie <schwokelbaer@gmail.com>
2022-07-09 22:58:36 +0200

    versuch mal so

--- Hauskauf.c
+++ Hauskauf.c
@@ -2,22 +2,22 @@
 
 int hauskauf(float sparen_emily, float sparen_sarah, int haus_kosten)
 {
-    int count = 0;
-    int prc = 0;
-    float percentage = ((1.5/100));
-    float result = 0;
-    result = sparen_emily + sparen_sarah;
-    
-    while(result <= haus_kosten)
-    {
-        result += percentage * result + 2000;
-        count++;
-        prc ++;
-        if(prc == 36)
-        {
-            percentage += (0.1/100);
-            prc = 0;
-        }
-    }
-    return count;
+	int		month = 0;
+	float	percent = 0.015;
+	float	money = 0;
+		
+	money = sparen_emily + sparen_sarah;
+	while (money <= haus_kosten)
+	{
+		if(month % 36 == 0 && month != 0)
+			percent += 0.001;
+		money += percent * money + 2000;
+		month++;
+	}
+	return (month);
+}
+
+int main()
+{
+    printf("-> %d\n", hauskauf(15000.0, 20000.42, 80000));
 }
