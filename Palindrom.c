346edc39304f6b86de9ecb52db7aa7a24cc4a948
marie <schwokelbaer@gmail.com>
2022-07-10 14:52:14 +0200

    see

--- Palindrom.c
+++ Palindrom.c
@@ -3,26 +3,31 @@
 #include <stdlib.h>
 
 
-int reverse(char* string, int laenge)
+int palindrom(char* string, int laenge)
 {
+        //printf("strlen: %lu\nlaenge: %d\n", strlen(string), laenge);
+    if(strlen(string) != laenge)
+        return(1);
     int i = 0;
-    char arr[11];
-    arr[10] = '\0';
+    printf("laenge: %d\n", laenge);
+    char arr[laenge];
+    arr[laenge] = '\0';
     while(i <= laenge)
     {
-        arr[i] = string[laenge - i];
+        printf("len(%d)-i(%d); %c\n",laenge, i, string[laenge-1-i]);
+        arr[i] = string[laenge -1- i];
         i++;
     }
-    puts(arr);
+    printf("arr: %s\nstring: %s\n", arr, string);
     return strcmp(arr, string);
 }
 
 int main(void)
 {
-    char string[10] = "lagerregal";
-    int laenge = 9;
-   // scanf("%s %d", string, &laenge);
-    if(reverse(string, laenge) == 0)
+    char *string;
+    int laenge;
+    scanf("%s %d", string, &laenge);
+    if(palindrom(string, laenge) == 0)
     {
         printf("palindrom");
     }
