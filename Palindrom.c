6be9e4f1ff1ebebd851369a5fae059a0bad82bbc
marie <schwokelbaer@gmail.com>
2022-07-10 16:32:45 +0200

    see

--- Palindrom.c
+++ Palindrom.c
@@ -3,30 +3,55 @@
 #include <stdlib.h>
 
 
+int strcmp_A_a(char *str, char *arr)
+{
+    int i = 0;
+    int count = 0;
+    while (str[i] != '\0')
+    {
+        if(str[i] == arr[i])
+            count++;
+        else if(str[i] >= 65 && str[i] <= 90)
+        {
+            if(arr[i] - 32 == str[i])
+                count++;
+        }
+        else if(str[i] >= 97 && str[i] <= 122)
+        {
+            if(arr[i] + 32 == str[i])
+                count++;
+        }
+        i++;
+    }
+    if(count == (int)strlen(str))
+        return (0);
+    else
+        return (1);
+}
+
 int palindrom(char* string, int laenge)
 {
-        //printf("strlen: %lu\nlaenge: %d\n", strlen(string), laenge);
-    if(strlen(string) != laenge)
+    if((int)strlen(string) != laenge)
         return(1);
     int i = 0;
-    printf("laenge: %d\n", laenge);
     char arr[laenge];
     arr[laenge] = '\0';
     while(i <= laenge)
     {
         printf("len(%d)-i(%d); %c\n",laenge, i, string[laenge-1-i]);
-        arr[i] = string[laenge -1- i];
+        arr[i] = string[laenge - 1 - i];
         i++;
     }
     printf("arr: %s\nstring: %s\n", arr, string);
-    return strcmp(arr, string);
+
+    return strcmp_A_a(string, arr);
 }
 
 int main(void)
 {
-    char *string;
+    char string[1024];
     int laenge;
-    scanf("%s %d", string, &laenge);
+    scanf("%1023s %d", string, &laenge);
     if(palindrom(string, laenge) == 0)
     {
         printf("palindrom");
@@ -34,3 +59,4 @@ int main(void)
     else
         printf("kein palindrom");
 }
+
