d0aeb1c6f5b842580c8130c9fcd0e9d4a795b646
marie <schwokelbaer@gmail.com>
2022-07-11 15:13:36 +0200

    yee

--- ggT.c
+++ ggT.c
@@ -4,44 +4,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-unsigned long ggT (unsigned long a, unsigned long b)
+unsigned long ggT(unsigned long a, unsigned long b) 
 {
-    if( (a % b) > 0 && a > b )
-    {
-        return ggT(b, a % b );
-    }
-    
-    if( (b % a) > 0 && b > a )
-    {
-        return ggT(a, b % a );
-    }
-    
-    if( a % b == 0 && b < a)
-    {
-        return b;
-    }
-    if(a % b == 0 && b > a)
-    {
-        return a;
-    }
-    if( b % a == 0 && b < a)
-    {
-        return b;
-    }
-    else if(b % a == 0 && b > a)
-    {
-        return a;
-    }
-    
-    if( b == 0 )
-    {
-        return a;
-    }
-    
-    if( a == 0 )
-    {
-        return b;
-    }
-
-    return ggT(a,b);
+    if (b == 0) 
+        return (a);
+    return (ggT(b, a % b));
 }
