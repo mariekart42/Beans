62f2abd96e1a8cfcc63ef44ab19ef09dd78485bc
marie <schwokelbaer@gmail.com>
2022-07-11 14:10:29 +0200

    yee

--- alphabet.c
+++ alphabet.c
@@ -2,44 +2,52 @@
 #include <string.h>
 #include <stdlib.h>
 
+int return_place(char c, char *ptr)
+{
+	int i = 0;
+	while(ptr[i] != '\0')
+	{
+		if(ptr[i] == c)
+			return(i);
+		i++;
+	}
+	return(0);
+}
 
 char *substitute(const char *source, const char *target, const char *text)
 {
     int i = 0;
-    int j = 0;
     char *arrout;
-    arrout = char *malloc( strlen(text) * sizeof(char) + 1);
-    
-    unsigned long length_text = strlen(text);
-    unsigned long length_source = strlen(source);
-    unsigned long length_target = strlen(target);
-    
-    if(length_source != length_target)
-    {
-        return NULL;
-    }
-    
-    if(length_text > length_target)
-    {
-        return NULL;
-    }
-    
-    
-    
-    while(i < int length_text)
-    {
-        while(j < length_source)
-        {
-            if(text[i] == source[j])
-            {
-                 arrout[i] = target[j];
-            }
-            j++;
-        }
-        i++;
-    }
-    
-    return arrout;
+    arrout = (char *)malloc(strlen(text) * sizeof(char) + 1);
+    if(!arrout)
+        return(NULL);
+
+	//checkt ob a alles was in text auch in source ist
+	while(text[i] != '\0')
+	{
+		if(strchr(source, text[i]) == NULL)
+			return(NULL);
+		i++;
+	}
+
+	i = 0;
+	int place = 0;
+	while (text[i] != '\0')
+	{
+		place = return_place(text[i], (char *)source);
+		arrout[i] = target[place];
+		i++;
+	}
+
+    arrout[i] = '\0';
+    return (arrout);
 }
 
 
+// int main(void)
+// {
+// 	char *str;
+// 	str = substitute("012345", "ABCDEF", "1245");
+// 	printf("str: %s\n", str);
+// 	free(str);
+// }
